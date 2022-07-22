f5538c471726abea3c907a5f091f418e5b9c0892
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2022-07-22 12:03:21 +0100

    made a line graphically

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var axiom = "F";
 var sentence = axiom; // sentence is going to start with a axiom
+var len = 100;
 
 var rules = []
 
@@ -13,6 +14,9 @@ var rules = []
 //   a: "B",
 //   b: "A"
 // }
+
+// this is a command for our turtle to draw on.
+// F means forward, + means turn right, - means turn left, [ means save where you are, ] means save where you are ;
 
 rules[0] = {
   a: "F",
@@ -37,11 +41,36 @@ function generate() {
   }
   sentence = nextSentence;
   createP(sentence);
+  turtle();
+}
+
+function turtle() {
+  background(51);
+  translate(width/2, height);
+  stroke(255);
+  for (var i = 0; i < sentence.length; i++) {
+    var current = sentence.charAt(i);
+
+    if(current == 'F'){
+      line(0,0,0,-len);
+      translate(0, -len);
+    } else if(current == '+'){
+      rotate(PI/6);
+    } else if(current == '-'){
+      rotate(-PI/6);
+    } else if(current == '['){
+      push();
+    } else if(current == ']'){
+      pop();
+    }
+  }
 }
 
 function setup() {
-  noCanvas();
+  createCanvas(400,400)
+  background(51);
   createP(axiom);
+  turtle();
   var button = createButton("generate");
   button.mousePressed(generate)
 }
